8c7bf98cce566b1fc86fd769a8c96a8a3ef0a494
ScarletMartins <scarletgam@icloud.com>
2022-10-05 14:55:25 -0300

    criação camada model

---
+++ Projeto-Bem-Fazer/src/main/java/com/generation/bemfazer/BemfazerApplication.java
@@ -0,0 +1,13 @@
+package com.generation.bemfazer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemfazerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemfazerApplication.class, args);
+	}
+
+}
